208dd78e0682df1c23a5c07fa58e6ccaacfa8097
KOWSEKHAN <159884882+KOWSEKHAN@users.noreply.github.com>
2024-11-08 22:52:18 +0530

    Create 14.longest common prefix.py

---
+++ 14.longest common prefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            print("")
+        prefix=strs[0]    
+        for i in strs[1:]:
+            while not i.startswith(prefix):
+                prefix=prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix

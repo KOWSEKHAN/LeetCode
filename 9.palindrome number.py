cfe236955f695d3e94d421987f690da707e69f42
KOWSEKHAN <159884882+KOWSEKHAN@users.noreply.github.com>
2024-11-08 22:47:33 +0530

    Create 9.palindrome number.py

---
+++ 9.palindrome number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        a=str(x)
+        return a==a[::-1]
